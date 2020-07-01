a0ed41e3e2ed1ecfb1cd63458dee3d7105685ddb
Noah Lackey <z5169776@ad.unsw.edu.au>
2020-07-01 19:53:24 +1000

    Animate add task button

--- frontend/src/Popup.js
+++ frontend/src/Popup.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { CSSTransition } from 'react-transition-group'; 
 
 import './Popup.css';
 
 import Task from './Task.js';
 
+import addBtn from './assets/addIcon.png';
+
 export default class Popup extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+            showAddTask: false,
             tasks: ["Task 1", "Task 2"],
         };
     }
@@ -16,16 +19,24 @@ export default class Popup extends React.Component {
     render() {
     
         const taskItems = this.state.tasks.map(taskName => <Task taskName={taskName}/>); 
-    
+        
+        const animClassNames = {
+            enter: "addTaskEnter",
+            enterActive: "addTaskEnterActive",
+            exit: "addTaskExit",
+            exitActive: "addTaskExitActive"
+        };
+        
         return (
             <div className="popupContainer">
                 <div className="popupHeader">
-                    <p className="popupTitle"> Time Tracer </p>
-                    <div className="popupActions">
-                        <a className="popupAddTaskButton" href="#">
-                        
-                        </a>
-                    </div>
+                    Time Tracer
+                        <div className="popupActions">
+                            <a href="#"onClick={() => this.setState({showAddTask: !this.state.showAddTask})}>
+                                <img src={addBtn} className={"newTaskBtn" + (this.state.showAddTask ? "Hide" : "Show")}/>
+                            </a>
+                        </div>
+                    
                 </div>
                 <div className="popupBody">
                     <div className="taskList">
