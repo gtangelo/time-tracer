1850802007a4a63cb194ff94f626c6ceb776cb16
Noah Lackey <z5169776@ad.unsw.edu.au>
2020-07-03 21:41:07 +1000

    Update internal representation of task info

--- frontend/src/Popup.js
+++ frontend/src/Popup.js
@@ -1,37 +1,66 @@
+/* global chrome */
+
 import React from 'react';
-import { CSSTransition } from 'react-transition-group'; 
 
 import './Popup.css';
 
 import Task from './Task.js';
 
 import addBtn from './assets/addIcon.png';
+import tickBtn from './assets/tickIcon.png';
 
 export default class Popup extends React.Component {
     constructor(props) {
         super(props);
+        this.onToggle = this.onToggle.bind(this);
         this.state = {
             showAddTask: false,
-            tasks: ["Task 1", "Task 2"],
+            tasks: [{id: "Task 1", playing: false, time: 0, start: 0}],
         };
+    }
+    
+    onToggle(taskId) {
+        var tasks = this.state.tasks;
+        for (var i = 0; i < tasks.length; i++) {
+            if (tasks[i].id == taskId) {
+                if (tasks[i].playing == false) {
+                    tasks[i].playing = true;
+                    tasks[i].start = Date.now();
+                } else {
+                    tasks[i].playing = false;
+                    if (Date.now() - tasks[i].start > 60000) {
+                        tasks[i].time += Date.now() - tasks[i].start;
+                    }
+                    tasks[i].start = 0;
+                }
+                this.setState({tasks: tasks});
+                break;
+            }
+        }
+    }
+    
+    createNewTask() {
+        return;
     }
     
     render() {
     
-        const taskItems = this.state.tasks.map(taskName => <Task taskName={taskName}/>); 
+        const taskItems = this.state.tasks.map(task => <Task taskId={task.id} onToggle={this.onToggle}/>); 
         
         return (
             <div className="popupContainer">
                 <div className="popupHeader">
                     Time Tracer
                         <div className="popupActions">
-                            <a href="#"onClick={() => this.setState({showAddTask: !this.state.showAddTask})}>
+                            <a href="#" onClick={() => this.setState({showAddTask: !this.state.showAddTask})}>
                                 <img src={addBtn} className={"newTaskBtn" + (this.state.showAddTask ? "Hide" : "Show")}/>
                             </a>
                         </div>
                 </div>
                 <div className={"newTaskMenu" + (this.state.showAddTask ? "Show" : "Hide")}>
-                    
+                    <a href="#" onClick={this.createNewTask()}>
+                        <img src={tickBtn} className={"tickBtn"}/>
+                    </a>
                 </div>
                 <div className="popupBody">
                     <div className="taskList">
--- frontend/src/Task.js
+++ frontend/src/Task.js
@@ -5,22 +5,32 @@ import './Task.css';
 import infoBtn from './assets/infoIcon.png';
 import delBtn from './assets/deleteIcon.png';
 import playBtn from './assets/playIcon.png';
+import pauseBtn from './assets/deleteIcon.png';
 
 export default class Task extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {playing: false};
+        this.onToggle = this.onToggle.bind(this);
+    }
+    
+    onToggle() {
+        this.setState({playing: !this.state.playing});
+        this.props.onToggle(this.props.taskId);
     }
     
     render() {
         return (
             <div className="taskContainer">
                 <div className="taskInfo">
-                    {this.props.taskName}
+                    {this.props.taskId}
                 </div>
                 <div className="taskActions">
                     <img src={infoBtn} className="taskActionBtn"/>
                     <img src={delBtn}  className="taskActionBtn"/>
-                    <img src={playBtn} className="taskActionBtn"/>
+                    <a href="#" onClick={this.onToggle}>
+                    <img src={this.state.playing ? pauseBtn : playBtn} className="taskActionBtn"/>
+                    </a>
                 </div>
             </div>
         );
