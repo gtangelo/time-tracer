6c2cc46430cde3fa8791db975ef93bbb5136cf66
gtangelo <65492346+gtangelo@users.noreply.github.com>
2020-07-10 01:38:02 +1000

    update form design

--- frontend/src/TaskForm.js
+++ frontend/src/TaskForm.js
@@ -24,61 +24,65 @@ export default class TaskForm extends React.Component {
     }
 
     render() {
-
         return (
-            <>
-        <div id = "task">
-                <input
-                    type="text"
-                    name="taskName"
-                    placeholder="Task Name"
-                    onChange={(e) => this.setState({ taskID: e.target.value })}
-                />
-                  <button onClick={this.handleSubmitForm}> </button>
-        </div>
+        <form>
         <div id = "fields">
-            <label for="empID">Emp ID:</label>
-                <input
-                    type="text"
-                    name="empID"
-                    onChange={(e) => this.setState({ empID: e.target.value })}
-                />
-              
-           
-                <label for="wbsName">WBS:</label>
-                <select
-                    name="wbsName"
-                    onChange={(e) => this.setState({ wbs: e.target.value })}>
-                    <option value="wbs1"> WBS 1</option>
-                    <option value="wbs2"> WBS 2</option>
-                    <option value="wbs3"> WBS 3</option>
-                </select>
-           
-          
-                <label for="teamName">Team:</label>
-                <select
-                    name="teamName"
-                    onChange={(e) => this.setState({ teamName: e.target.value })}>
-                    <option value="team1"> Team 1</option>
-                    <option value="team2"> Team 2</option>
-                    <option value="wbs3"> Team 3</option>
-                </select>
-          
-                <label for="clientName">Client</label>
-                <select
-                    name="clientName"
-                    onChange={(e) => this.setState({ clientName: e.target.value })}>
-                    <option value="ausGov"> Australian Gov</option>
-                    <option value="mcdonalds"> McDonald's</option>
-                    <option value="volvo"> Volvo</option>
-                    <option value="ericsson"> Ericsson</option>
-                    <option value="nestle"> Nestlé </option>
-                    <option value="volkswagen"> Volkswagen </option>
-                    <option value="nokia"> Nokia </option>
-                </select>
-            </div>  
-              
-            </>
+            <label for="taskName">Task Name</label>
+            <input
+                type="text"
+                name="taskName"
+                placeholder="Task 1"
+                onChange={(e) => this.setState({ taskID: e.target.value })}
+            />
+
+            <label for="clientName">Client</label>
+            <select
+                name="clientName"
+                placeholder="John Smith"
+                onChange={(e) => this.setState({ clientName: e.target.value })}>
+                <option value="" disabled selected>-- Select Option --</option>
+                <option value="ausGov"> Australian Gov</option>
+                <option value="mcdonalds"> McDonald's</option>
+                <option value="volvo"> Volvo</option>
+                <option value="ericsson"> Ericsson</option>
+                <option value="nestle"> Nestlé </option>
+                <option value="volkswagen"> Volkswagen </option>
+                <option value="nokia"> Nokia </option>
+            </select>
+
+            <label for="teamName">Team:</label>
+            <select
+                name="teamName"
+                placeholder="Team X"
+                onChange={(e) => this.setState({ teamName: e.target.value })}>
+                <option value="" disabled selected>-- Select Option --</option>
+                <option value="team1"> Team 1</option>
+                <option value="team2"> Team 2</option>
+                <option value="team3"> Team 3</option>
+            </select>
+            
+            <label for="empID">EID:</label>
+            <input
+                type="text"
+                name="empID"
+                placeholder="#12345"
+                onChange={(e) => this.setState({ empID: e.target.value })}
+            />
+
+            <label for="wbsName">WBS:</label>
+            <select
+                name="wbsName"
+                onChange={(e) => this.setState({ wbs: e.target.value })}>
+                <option value="" disabled selected>-- Select Option --</option>
+                <option value="wbs1"> WBS 1</option>
+                <option value="wbs2"> WBS 2</option>
+                <option value="wbs3"> WBS 3</option>
+            </select>
+        </div>
+        <div id = "task">
+            <button onClick={this.handleSubmitForm}>Create Task</button>
+        </div>
+        </form>
         );
     }
 
