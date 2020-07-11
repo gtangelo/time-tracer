86212e19d24dbb0a09fb0bf6efa52558e26be86e
elizabetht94 <elizabeth.laham@gmail.com>
2020-07-11 23:37:57 +1000

    Added form validation

--- frontend/src/TaskForm.js
+++ frontend/src/TaskForm.js
@@ -1,26 +1,57 @@
 import React from 'react';
 import './TaskForm.css';
 
+const initialState = {
+    empID: '',
+    taskID: '', 
+    wbs: '',
+    clientName: '',
+    teamName: '',
+    taskError: "",
+    empError: ""
+}
+
 export default class TaskForm extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {
-            empID: '',
-            taskID: '',
-            wbs: '',
-            clientName: '',
-            teamName: ''
-        };
+        this.state = {initialState};
         this.handleSubmitForm = this.handleSubmitForm.bind(this);
     }
 
+    validate() {
+        let taskError = "";
+        let empError = "";
+
+        if(!this.state.empID) {
+            empError = "Employee ID cannot be blank"
+        }
+
+        if (this.state.taskID.length < 2) {
+            taskError = "Task name must contain at least 2 characters"
+        }
+
+        if (taskError || empError) {
+            this.setState({taskError, empError});
+            return false;
+        }
+    
+        return true;
+    }
+
     handleSubmitForm() {
-        this.props.onSubmit(this.state.taskID, {
-            empID: this.state.empID,
-            wbs: this.state.wbs,
-            clientName: this.state.clientName,
-            teamName: this.state.teamName
-        });
+        const isValid = this.validate();
+        if (isValid) {
+            this.props.onSubmit(this.state.taskID, {
+                empID: this.state.empID,
+                wbs: this.state.wbs,
+                clientName: this.state.clientName,
+                teamName: this.state.teamName,
+                
+            });
+            // Clear the form, which will allow the user to resubmit after
+            // correcting errors
+            this.setState(initialState);
+        }
     }
 
     render() {
@@ -35,15 +66,18 @@ export default class TaskForm extends React.Component {
                     onChange={(e) => this.setState({ taskID: e.target.value })}
                 />
                   <button onClick={this.handleSubmitForm}> </button>
+                  <div style={{fontSize: 10, color: "red"}}> {this.state.taskError}</div>
         </div>
         <div id = "fields">
+            <div>
             <label for="empID">Emp ID:</label>
                 <input
                     type="text"
                     name="empID"
                     onChange={(e) => this.setState({ empID: e.target.value })}
                 />
-              
+                <div style={{fontSize: 10, color: "red"}}> {this.state.empError}</div>
+              </div>
            
                 <label for="wbsName">WBS:</label>
                 <select
