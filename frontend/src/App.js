bc305f04824d9b0c84bee38b58639a3053516566
elizabetht94 <elizabeth.laham@gmail.com>
2020-07-15 00:00:03 +1000

    merging

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,21 @@ import React from 'react';
 import './App.css';
 import Popup from './Popup.js';
 
+
+/** 
+ * 
+ * =================================================================================
+ * SLP WINTER 2020 - Time Tracer Extension
+ * Industry Partner: Accenture
+ * Team: Avanti Aswal, Elizabeth Laham, Gabriel Ting, Noah Lackey, Patricia Nguyen
+ * =================================================================================
+ * 
+ *  */
+
+
+
+
+
 function App() {
   return (
     <Popup/>
